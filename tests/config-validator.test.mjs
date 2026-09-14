e6c77fef901c0af164c1054763b61a53307d0ee2
MI <59292793+ion-motica@users.noreply.github.com>
2026-09-14 16:39:25 +0300

    Add config validator tests

---
+++ tests/config-validator.test.mjs
@@ -0,0 +1,35 @@
+import assert from "node:assert/strict";
+import { validateProtectie, validateRemindere } from "../Hook UserPromptSubmit/config-validator.mjs";
+
+const protectOk = `
+/* comment */
+blocheazaEditareFisiereSiFoldere {
+  js/a.js
+  js/core/
+}
+permiteEditarePanaLa(2026.09.15-18.00 Europe/Bucharest) {
+  js/a.js
+}
+permiteEditareTOT_REPOSITORYPanaLa(2026.09.15-18.00 Europe/Bucharest) {
+}
+`;
+assert.deepEqual(validateProtectie(protectOk), []);
+assert.ok(validateProtectie("blocheazaEditareFisiereSiFoldere {\n js/a.js\n").length > 0);
+assert.ok(validateProtectie("permiteEditarePanaLa(2026.99.99-88.99 Europe/Bucharest) {}\n").length > 0);
+
+const remindersOk = `
+/*
+insereazaInToatePrompturile {{ dezactivat }}
+*/
+dacaGaseste(CP | control panel | panou de control)
+atunciIncludeInPrompt {{Text cu \"ghilimele\".}}
+
+dacaGaseste(crc)
+atunciIncludeInPrompt {{Alt text.}}
+`;
+assert.deepEqual(validateRemindere(remindersOk), []);
+assert.ok(validateRemindere("dacaGaseste(CP) atunciIncludeInPrompt {{ text\n").length > 0);
+assert.ok(validateRemindere("dacaGaseste(CP | ) atunciIncludeInPrompt {{text}}\n").length > 0);
+assert.ok(validateRemindere("dacaGaseste(CP) altceva {{text}}\n").length > 0);
+
+console.log("TESTE OK");
