38b119f7179f61e1447151c08f6d49e465544a82
MI <59292793+ion-motica@users.noreply.github.com>
2026-09-16 20:25:30 +0300

    Actualizeaza testul validatorului pentru noul nume al folderului hook

--- tests/config-validator.test.mjs
+++ tests/config-validator.test.mjs
@@ -1,5 +1,5 @@
 import assert from "node:assert/strict";
-import { validateProtectie, validateRemindere } from "../Hook UserPromptSubmit - cand trimit prompt/config-validator.mjs";
+import { validateProtectie, validateRemindere } from "../Hook UserPromptSubmit - cand trimit prompt insereaza reminder/config-validator.mjs";
 
 const protectOk = `
 /* comment */
